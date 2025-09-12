5935058f24e952e3c3725a000572fc50016424c8
Lueur3 <sgdm515@gmail.com>
2025-09-12 13:07:31 +0300

    added border and net settings

--- settings.py
+++ settings.py
@@ -2,5 +2,15 @@
 class Settings:
 
     def __init__(self):
-        self.sreen_width = 1200
-        self.sreen_height = 800
+        self.screen_width = 1200
+        self.screen_height = 800
+
+        self.border_width = self.screen_width
+        self.border_height = 20
+
+        self.net_width = 15
+        self.net_height = self.screen_height
+
+
+
+        self.game_active = False
