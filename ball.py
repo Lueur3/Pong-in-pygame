a9d59063c837ee6c884a50bb0a7809b22403db3b
Lueur3 <sgdm515@gmail.com>
2025-09-14 15:07:58 +0300

    added corrected check_paddle

--- ball.py
+++ ball.py
@@ -2,12 +2,15 @@ import pygame
 from pygame.sprite import Sprite
 from random import randint
 
+
 class Ball(Sprite):
 
     def __init__(self, pn_game):
         super().__init__()
         self.screen = pn_game.screen
         self.settings = pn_game.settings
+        self.r_paddle_y = pn_game.r_paddle
+        self.l_paddle_y = pn_game.l_paddle
 
         self.image = pygame.Surface([self.settings.ball_size,
                                      self.settings.ball_size])
@@ -23,6 +26,13 @@ class Ball(Sprite):
         self.x = self.settings.screen_width // 2 - self.settings.net_width
 
 
+        self._ball_pos()
+
+
+        self.rect.y = int(self.y)
+        self.rect.x = int(self.x)
+
+    def _ball_pos(self):
         position = randint(1, 2)
 
         if position == 2:
@@ -30,9 +40,6 @@ class Ball(Sprite):
         elif position == 1:
             self.x -= 100
 
-
-        self.rect.y = int(self.y)
-        self.rect.x = int(self.x)
 
     def _check_bounds(self):
 
@@ -49,18 +56,21 @@ class Ball(Sprite):
 
     def _check_paddle(self):
 
-        if (self.x >= (self.settings.screen_width - self.settings.paddle_width - self.settings.ball_size)
-                and self.x >= self.settings.paddle_width):
+        if (self.x >= self.settings.screen_width - self.settings.paddle_width - self.settings.ball_size
+                and self.r_paddle_y.rect.y <= self.y <= self.r_paddle_y.rect.y + self.settings.paddle_height):
+
             self.settings.ball_dx *= -1
             self.settings.ball_speed_x *= self.settings.ball_dx
             self.settings.ball_dy *= -1
             self.settings.ball_speed_y *= self.settings.ball_dy
 
-        elif self.x <= self.settings.paddle_width:
+        elif (self.x <= self.settings.paddle_width
+              and self.l_paddle_y.rect.y <= self.y <= self.l_paddle_y.rect.y + self.settings.paddle_height):
             self.settings.ball_dx *= -1
             self.settings.ball_speed_x *= self.settings.ball_dx
             self.settings.ball_dy *= -1
             self.settings.ball_speed_y *= self.settings.ball_dy
+
 
     def update(self):
         self._check_bounds()
