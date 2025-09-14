2ccfe54504821ea70696517ab441536c4482d6c5
Lueur3 <sgdm515@gmail.com>
2025-09-14 15:21:44 +0300

    changed logic collision

--- ball.py
+++ ball.py
@@ -9,8 +9,8 @@ class Ball(Sprite):
         super().__init__()
         self.screen = pn_game.screen
         self.settings = pn_game.settings
-        self.r_paddle_y = pn_game.r_paddle
-        self.l_paddle_y = pn_game.l_paddle
+        self.r_paddle = pn_game.r_paddle
+        self.l_paddle = pn_game.l_paddle
 
         self.image = pygame.Surface([self.settings.ball_size,
                                      self.settings.ball_size])
@@ -26,14 +26,16 @@ class Ball(Sprite):
         self.x = self.settings.screen_width // 2 - self.settings.net_width
 
 
-        self._ball_pos()
+        self.ball_pos()
 
 
         self.rect.y = int(self.y)
         self.rect.x = int(self.x)
 
-    def _ball_pos(self):
+    def ball_pos(self):
         position = randint(1, 2)
+        self.y = self.settings.screen_height // 2
+        self.x = self.settings.screen_width // 2 - self.settings.net_width
 
         if position == 2:
             self.x += 100
@@ -56,16 +58,13 @@ class Ball(Sprite):
 
     def _check_paddle(self):
 
-        if (self.x >= self.settings.screen_width - self.settings.paddle_width - self.settings.ball_size
-                and self.r_paddle_y.rect.y <= self.y <= self.r_paddle_y.rect.y + self.settings.paddle_height):
-
+        if pygame.sprite.collide_rect(self, self.r_paddle):
             self.settings.ball_dx *= -1
             self.settings.ball_speed_x *= self.settings.ball_dx
             self.settings.ball_dy *= -1
             self.settings.ball_speed_y *= self.settings.ball_dy
 
-        elif (self.x <= self.settings.paddle_width
-              and self.l_paddle_y.rect.y <= self.y <= self.l_paddle_y.rect.y + self.settings.paddle_height):
+        elif pygame.sprite.collide_rect(self, self.l_paddle):
             self.settings.ball_dx *= -1
             self.settings.ball_speed_x *= self.settings.ball_dx
             self.settings.ball_dy *= -1
@@ -75,11 +74,9 @@ class Ball(Sprite):
     def update(self):
         self._check_bounds()
         self._check_paddle()
+
         self.y += self.settings.ball_speed_y
         self.x += self.settings.ball_speed_x
-
-
-
 
         self.rect.y = int(self.y)
         self.rect.x = int(self.x)
